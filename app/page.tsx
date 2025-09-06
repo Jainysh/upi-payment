0d9ebf0d8e081ac07597db3bd2740e1e6ddb46e6
Yash Jain <yjain56@yahoo.com>
2025-09-06 13:13:50 +0530

    chore: og url updated

--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,11 @@ export const metadata: Metadata = {
     title: "Camp on Self Defence",
     description:
       "Join our camp on self defence conducted by expert trainers at Sri Adinath Jain Mandir, Chickpet, Bangalore",
-    url: "https://payment.yashjain.xyz",
+    url: "https://self-defence.vercel.app",
     siteName: "Sri Adinath Jain Sangh",
     images: [
       {
-        url: "https://payment.yashjain.xyz/assets/logo.png",
+        url: "https://self-defence.vercel.app/assets/logo.png",
         width: 1200,
         height: 630,
         alt: "Camp on Self Defence",
