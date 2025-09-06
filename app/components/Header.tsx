4369c18b04a71a71292f9bc92287afbe3d7f239a
Yash Jain <yjain56@yahoo.com>
2025-09-06 12:21:34 +0530

    feat: og added

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,8 +1,18 @@
 "use client";
 
 import { Box, Typography, Container } from "@mui/material";
+import { useEffect, useState } from "react";
 
 export const Header = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null; // or a placeholder that matches server HTML
+  }
   return (
     <Box
       component="header"
